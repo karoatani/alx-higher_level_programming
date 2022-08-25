ec8bcdc3b8401a2a43aa3e16f7cbe0404ed16fc3
karo atani <karoatani@gmail.com>
2022-08-25 15:25:41 +0100

    added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 length = len(sys.argv)
 if __name__ == '__main__':
     if length == 1:
-        print('0 arguments.')
+        print('{}'.format('0 arguments.'))
     else:
         print('{} arguments:'.format(length - 1))
         for index, i in enumerate(sys.argv[1:], 1):
