93fd48d349c929f0cacb455f29ab16f2629dd46c
karo atani <karoatani@gmail.com>
2022-08-27 10:14:53 +0100

    added

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ from copy import copy
 
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > (len(my_list) - 1):
         return my_list.copy()
     cp_list = my_list[:]
     for i in cp_list:
