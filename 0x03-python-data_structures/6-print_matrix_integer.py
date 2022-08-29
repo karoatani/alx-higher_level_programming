02e2dfcd819c8af437ff16cc0b098c36a028b291
karo atani <karoatani@gmail.com>
2022-08-29 02:29:37 +0100

    added

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,25 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for x in i:
             print('{:d} '.format(x), end='')
-        print('{}'.format(''))
+        print()
+
+
+def gt(matrix=[[]]):
+    for i in matrix:
+        print(' '.join('{:d}'.format(j)for j in i))
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
+
+
+gt(matrix)
+print("--")
+gt()
