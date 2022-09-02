14e49b3a1cdb908bd2efa2e62d0d46b52e0c1e6e
karoatani <92018430+karoatani@users.noreply.github.com>
2022-09-02 03:54:29 -0700

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    return dict(filter(lambda v: value not in v, a_dictionary.items()))
+    if value in a_dictionary.values():
+        store_key = []
+        for k, v in a_dictionary.items():
+            if v == value:
+                store_key.append(k)
+        for i in store_key:
+            del a_dictionary[i]
+        return a_dictionary
+    return a_dictionary
+# return dict(filter(lambda v: value not in v, a_dictionary.items()))
