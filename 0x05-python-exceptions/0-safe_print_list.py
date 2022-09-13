6ff98463c7115a876aad1c83580cb25401556b11
karo atani <karoatani@gmail.com>
2022-09-13 23:36:24 +0100

    added

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     if type(value) == int:
-        print()
+        print("{:d}".format(value))
         return True
     else:
         try:
-             value = int(value)
+            value = int(value)
         except ValueError:
             return False
         print("{:d}".format(value))
