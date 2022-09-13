500072dac5660655ee98badfad4a39cf54b6ddf4
karo atani <karoatani@gmail.com>
2022-09-14 00:27:17 +0100

    added

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+""" Define a class Square """
+
+
+class Square:
+    """A square class model"""
+
+    def __init__(self, size=0):
+        """Initialized a square
+        Args:
+            size (:obj:`int`, optional): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
