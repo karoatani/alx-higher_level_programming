6603888d4664327c3bde837b3111b1890a7b864c
karo atani <karoatani@gmail.com>
2022-09-14 00:35:49 +0100

    added

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" Define a class Square """
+
+
+class Square:
+    """A square class model"""
+
+    def __init__(self, size=0):
+        """Initialized a square
+        Args:
+            size (int): size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """Calculate the area"""
+        return self.__size * self.__size
