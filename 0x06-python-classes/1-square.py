345adcab425c9f60aa36d07ddea93309e1ded86f
karo atani <karoatani@gmail.com>
2022-09-14 00:15:32 +0100

    added

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,4 +6,8 @@ class Square:
     """A square class model"""
 
     def __init__(self, size):
+        """
+        Args:
+            size (int): size of the square.
+        """
         self.__size = size
