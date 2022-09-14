986cf5cbf61f7c298c54b8dbcbcacf4946345571
karoatani <92018430+karoatani@users.noreply.github.com>
2022-09-14 08:17:56 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-def _len(li):
-    num = 0
-    for i in li:
-        num += 1
-    return num
-
-
 def safe_print_list(my_list=[], x=0):
-    try:
-        [print(my_list[index], end='') for index, i in enumerate(range(x))]
-    except IndexError:
-        return _len(my_list)
-    finally:
-        print()
-    return x
+    index = 0
+    while True:
+        try:
+            if index < x:
+                print(my_list[index], end='')
+                index += 1
+            else:
+                print()
+                return index
+        except IndexError:
+            print()
+            return index
