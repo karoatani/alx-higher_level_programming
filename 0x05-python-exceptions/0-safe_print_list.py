93882814dd2c6abd4f8c6aed80dfd9b3be09c47c
karo atani <karoatani@gmail.com>
2022-09-14 03:37:59 +0100

    added

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+def _len(li):
+    num = 0
+    for i in li:
+        num += 1
+    return num
+
+
 def safe_print_list(my_list=[], x=0):
     try:
         [print(my_list[index], end='') for index, i in enumerate(range(x))]
     except IndexError:
-        return len(my_list)
+        return _len(my_list)
     finally:
         print()
     return x
