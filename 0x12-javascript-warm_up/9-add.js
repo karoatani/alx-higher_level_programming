1e83135022ab4894f8cf486901df30daf12e3228
karo atani <karoatani@gmail.com>
2022-11-30 04:42:59 +0100

    added

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 function add(a, b) {
-  if (process.argv.length == 4) console.log(parseInt(a) + parseInt(b));
+  if (process.argv.length == 4 && !isNaN(parseInt(process.argv[2])) && !isNaN(parseInt(process.argv[3]))) {
+    console.log(parseInt(a) + parseInt(b));
+  }
 }
 add(process.argv[2], process.argv[3]);
